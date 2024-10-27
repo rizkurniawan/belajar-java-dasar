d8b76b9b90a4c8193a65901b4c5e327b8ae7f521
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-27 12:47:28 +0700

    belajar tipe data string

---
+++ src/TipeDataString.java
@@ -0,0 +1,9 @@
+public class TipeDataString {
+    public static void main(String[] args) {
+        String firstName = "Rizky";
+        String lastName = "Kurniawan";
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+    }
+}
