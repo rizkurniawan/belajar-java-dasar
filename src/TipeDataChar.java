71b2fcfe6b24e756aab0565bfd87ce5ee70d0197
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-27 12:19:47 +0700

    belajar tipe data char

---
+++ src/TipeDataChar.java
@@ -0,0 +1,15 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+        char r = 'R';
+        char i = 'I';
+        char z = 'Z';
+        char k = 'K';
+        char y = 'Y';
+
+        System.out.print(r);
+        System.out.print(i);
+        System.out.print(z);
+        System.out.print(k);
+        System.out.print(y);
+    }
+}
