85fb8b9ffe0c586e5019848dc1b7140bf97e795b
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-27 18:37:13 +0700

    belajar operasi matematika dan perbandingan

---
+++ src/OperasiMatematika.java
@@ -0,0 +1,25 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+        // operator matematika
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        // augmented assignment
+        System.out.println(a += 1);
+        System.out.println(a -= 2);
+
+        // unary operator
+        int c = 100;
+
+        System.out.println(++c);
+        System.out.println(c--);
+        System.out.println(-c);
+        System.out.println(!true);
+    }
+}
