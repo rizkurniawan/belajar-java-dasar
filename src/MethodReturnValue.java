dcad34bf0d911e83054c3a802718f16f8c730f4c
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-28 20:23:25 +0700

    belajar method return value

---
+++ src/MethodReturnValue.java
@@ -0,0 +1,9 @@
+public class MethodReturnValue {
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+    }
+
+    static int sum(int valueA, int valueB) {
+        return valueA + valueB;
+    }
+}
