2a90185c616afba005f7f35090b075bf5abfbc8e
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-28 19:16:56 +0700

    belajar ternary operator

---
+++ src/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        var nilai = 80;
+
+        String ucapan = nilai >= 75 ? "Selamat anda lulus" : "Coba Lagi";
+
+        System.out.println(ucapan);
+    }
+}
