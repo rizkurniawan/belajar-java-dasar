0f934ee2feb06c11dd129c8e8ebcd4c5a31e70cb
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-29 21:39:53 +0700

    belajar komentar

---
+++ src/Komentar.java
@@ -0,0 +1,11 @@
+public class Komentar {
+    public static void main(String[] args) {
+        /**
+         * multi
+         * line
+         * comment
+         * */
+
+        // single line comment
+    }
+}
