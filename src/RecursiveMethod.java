593aa9bcdda31ee351a390b3fef27ca2868b0141
rizkurniawan <rizkykurniawan2023@gmail.com>
2024-10-29 21:19:47 +0700

    belajar recursive method

---
+++ src/RecursiveMethod.java
@@ -0,0 +1,26 @@
+public class RecursiveMethod {
+    public static void main(String[] args) {
+        System.out.println(factorialLoop(5));
+        System.out.println(factorialRecursive(5));
+    }
+
+    // factorial loop
+    static int factorialLoop(int value) {
+        var result = 1;
+        for (var counter = 1; counter <= value; counter++) {
+            result *= counter;
+        }
+        return result;
+    }
+
+    // factorial resursive
+    static int factorialRecursive(int value) {
+        if (value == 1) {
+            return 1;
+        } else {
+            return value * factorialRecursive(value-1);
+        }
+    }
+
+
+}
