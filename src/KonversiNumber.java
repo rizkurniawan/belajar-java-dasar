b7f9708367fc28b7bc3f71f017d3d2427fc301be
rizkurniawan <rizkykurniawan2023@gmail.com>
2025-11-25 09:27:13 +0700

    add Tipe Data Number and Konversi Number

---
+++ src/KonversiNumber.java
@@ -0,0 +1,11 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        // byte - short - int - long
+        byte age = 25;
+        int ageInt = age;
+
+        // long - int - short - byte
+        int ammount = 1000;
+        byte ammountByte = (byte) ammount;
+    }
+}
---
+++ src/TipeDataNumber.java
@@ -0,0 +1,18 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte age = 25;
+        short iniShort = 1000;
+        int uangSaya = 1000000;
+        long uangTemen = 1000000000L;
+
+        float phi = 3.14F;
+        double randNumber = 9.88888;
+
+        int whiteColorHex = 0xFFFFFF;
+        int binaryAge = 0b00011001;
+
+        long ammount = 1_000_000_000_000L;
+
+        System.out.println(ammount);
+    }
+}
